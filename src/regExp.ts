93bd7e00fb950f5a9b1590e271fb4d49a5240f4d
emi <emijesusgon@gmail.com>
2024-02-13 14:41:39 -0300

    add regular expressions file

---
+++ src/regExp.ts
@@ -0,0 +1,9 @@
+// export const userRegEx = ``;
+
+// regex.ts
+
+export const numbersRegExp: RegExp = /^[0-9]+$/;
+export const userRegExp: RegExp = /^[\w.]+$/;
+export const alphanumericRegExp: RegExp = /^[a-zA-Z0-9]+$/;
+export const emailRegExp: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+export const ipAddressRegExp: RegExp = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
